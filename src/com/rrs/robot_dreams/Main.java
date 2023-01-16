6fe1cf23c22035841af8b87a2339f2206fb9cdb2
Rud-Rostislav <rudrost.se@gmail.com>
2023-01-16 20:57:59 +0200

    first commit

---
+++ src/com/rrs/robot_dreams/Main.java
@@ -0,0 +1,14 @@
+package com.rrs.robot_dreams;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 6;
+        int b = 2;
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+
+    }
+}
