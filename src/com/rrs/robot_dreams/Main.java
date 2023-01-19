4b0619a6f3d116311b56139622ae2d6218cc4155
Rud-Rostislav <rudrost.se@gmail.com>
2023-01-19 20:08:05 +0200

    homework_4

--- src/com/rrs/robot_dreams/Main.java
+++ src/com/rrs/robot_dreams/Main.java
@@ -3,12 +3,12 @@ package com.rrs.robot_dreams;
 public class Main {
     public static void main(String[] args) {
 
-        int a = 6;
-        int b = 2;
-        System.out.println(a + " + " + b + " = " + (a + b));
-        System.out.println(a + " - " + b + " = " + (a - b));
-        System.out.println(a + " / " + b + " = " + (a / b));
-        System.out.println(a + " * " + b + " = " + (a * b));
+        String str = "Robot Dreams";
+        System.out.println("Оригінальний текст:\n" + str + "\n");
+        System.out.println("Верхній регістр:\n" + str.toUpperCase() + "\n");
+        System.out.println("Нижній регістр:\n" + str.toLowerCase() + "\n");
+        System.out.println("Перша буква:\n" + str.charAt(0) + "\n");
+        System.out.println("Остання буква:\n" + str.charAt(str.length() - 1) + "\n");
 
     }
 }
