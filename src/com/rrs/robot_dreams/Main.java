c996a9e3f224c80bc86f1182ab0cd9251a874565
Rud-Rostislav <rudrost.se@gmail.com>
2023-01-30 20:25:16 +0200

    homework 7

--- src/com/rrs/robot_dreams/Main.java
+++ src/com/rrs/robot_dreams/Main.java
@@ -3,38 +3,28 @@ package com.rrs.robot_dreams;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Числа від 1 до 10:");
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + ". ");
-        }
-
-        System.out.println("\n\nПарні числа від 1 до 100:");
-        for (int i = 1; i <= 100; i++) {
-            if (i % 2 != 0) {
-                continue;
-            }
-            System.out.print(i + ". ");
-        }
-
-        int factorial = 1;
-        for (int i = 1; i <= 7; i ++){
-            factorial *= i;
-        }
-        System.out.println("\n\nФакторіал числа 7: \n" + factorial + "\n");
-
-        System.out.println("Фібоначі до 100:");
-        int num0 = 1, num1 = 1, num2;
-        num2 = num0 + num1;
-        System.out.print(num0 + " " + num1 + " ");
-        while(num2 < 100) {
-            num2 = num0 + num1;
-            if (num2 > 100) {
-                break;
-            }
-            System.out.print(num2 + " ");
-            num0 = num1;
-            num1 = num2;
-        }
+        System.out.println(cube(3));
+        print_star(5);
+        print_element("&", 10);
 
     }
+
+    static int cube(int a) {
+        return a * a * a;
+    }
+
+    static void print_star(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    static void print_element(String element, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(element);
+        }
+        System.out.println();
+    }
+
 }
