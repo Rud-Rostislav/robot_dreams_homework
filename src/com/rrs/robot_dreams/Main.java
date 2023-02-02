085d9db655306ee2eec8576ade7c4b263b6c296e
Rud-Rostislav <rudrost.se@gmail.com>
2023-02-02 20:34:11 +0200

    homework 8

--- src/com/rrs/robot_dreams/Main.java
+++ src/com/rrs/robot_dreams/Main.java
@@ -1,30 +1,72 @@
 package com.rrs.robot_dreams;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(cube(3));
-        printStar(5);
-        printElement("&", 15);
+        int[] arr = new int[10];
+        createArrayRandom(arr);
+        printArray(arr);
+        minValue(arr);
+        maxValue(arr);
+        sumAllValue(arr);
+        averageValue(arr);
 
     }
 
-    static int cube(int a) {
-        return a * a * a;
-    }
-
-    static void printStar(int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print("*");
+    static void createArrayRandom(int[] arr) {
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(1,10);
         }
-        System.out.println();
     }
 
-    static void printElement(String element, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(element);
+    static void printArray(int[] arr) {
+        System.out.println("Масив з 10 елементів:");
+        for (int j : arr) {
+            System.out.print(j + "; ");
         }
-        System.out.println();
+    }
+
+    static void minValue(int[] arr) {
+        System.out.println("\nМінімальне значення:");
+        int min = arr[0];
+        for (int j : arr) {
+            if (j < min) {
+                min = j;
+            }
+        }
+        System.out.println(min);
+    }
+
+    static void maxValue(int[] arr) {
+        System.out.println("Максимальне значення:");
+        int max = arr[0];
+        for (int j : arr) {
+            if (j > max) {
+                max = j;
+            }
+        }
+        System.out.println(max);
+    }
+
+    static void sumAllValue(int[] arr) {
+        System.out.println("Сума всіх елементів:");
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        System.out.println(sum);
+    }
+
+    static void averageValue(int[] arr) {
+        System.out.println("Cереднє значення елементів:");
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        int avr = sum / arr.length;
+        System.out.println(avr);
     }
 
 }
