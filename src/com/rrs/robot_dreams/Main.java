25370a33ae9fa8ca3e62660fd3fac0a4e63ef409
Rud-Rostislav <rudrost.se@gmail.com>
2023-02-22 01:33:21 +0200

    homework 13

--- src/com/rrs/robot_dreams/Main.java
+++ src/com/rrs/robot_dreams/Main.java
@@ -1,14 +1,29 @@
 package com.rrs.robot_dreams;
 
+import com.rrs.robot_dreams.domain.Employee;
+import com.rrs.robot_dreams.service.EmployeeService;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car car1 = new Car("BMW", "Black", 2020);
-        Car car2 = new Car("Mercedes", "Gray", 2017);
-        Car car3 = new Car("Audi", "White", 2018);
-        Car car4 = new Car("BMW", "Black", 2020);
+        EmployeeService employeeService = new EmployeeService();
+        employeeService.printEmployees();
 
-        System.out.println(car1);
+        System.out.println();
+
+        Employee employeeWithMaxSalary = employeeService.findEmployeeMaxSalary(employeeService.findAll());
+        System.out.println("Max salary: ");
+        employeeService.printEmployee(employeeWithMaxSalary);
+
+        System.out.println();
+
+        Employee employeeWithMinSalary = employeeService.findEmployeeMinSalary(employeeService.findAll());
+        System.out.println("Min salary: ");
+        employeeService.printEmployee(employeeWithMinSalary);
+
+        System.out.println();
+
+        System.out.println("Taxes: " + employeeService.calculateTaxes(employeeService.findAll()));
 
     }
 }
