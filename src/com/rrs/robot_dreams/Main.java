b6ed9a3844f87efc9b1971208f1575f52132bbfb
Rud-Rostislav <rudrost.se@gmail.com>
2023-03-06 20:26:56 +0200

    homework_17

--- src/com/rrs/robot_dreams/Main.java
+++ src/com/rrs/robot_dreams/Main.java
@@ -3,12 +3,14 @@ package com.rrs.robot_dreams;
 public class Main {
     public static void main(String[] args) {
 
-        String str = "Robot Dreams";
-        System.out.println("Оригінальний текст:\n" + str + "\n");
-        System.out.println("Верхній регістр:\n" + str.toUpperCase() + "\n");
-        System.out.println("Нижній регістр:\n" + str.toLowerCase() + "\n");
-        System.out.println("Перша буква:\n" + str.charAt(0) + "\n");
-        System.out.println("Остання буква:\n" + str.charAt(str.length() - 1) + "\n");
+        System.out.println("Перший день тижня - " + DayOfWeek.getDayOfWeek(1) + ".");
+        System.out.println("Другий день тижня - " + DayOfWeek.getDayOfWeek(2) + ".");
+        System.out.println("Третій день тижня - " + DayOfWeek.getDayOfWeek(3) + ".");
+        System.out.println("Четвертий день тижня - " + DayOfWeek.getDayOfWeek(4) + ".");
+        System.out.println("П'ятий день тижня - " + DayOfWeek.getDayOfWeek(5) + ".");
+        System.out.println("Шестий день тижня - " + DayOfWeek.getDayOfWeek(6) + ".");
+        System.out.println("Сьомий день тижня - " + DayOfWeek.getDayOfWeek(7) + ".");
+        
 
     }
 }
